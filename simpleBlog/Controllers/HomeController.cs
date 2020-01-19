c91c767d65c17d46fce48457b4229f43ca58d094
Yididya-K <kebedeyididya1@gmail.com>
2020-01-19 20:40:03 +0300

    Asp.net example

---
+++ simpleBlog/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using simpleBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simpleBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BlogDBEntities _db = new BlogDBEntities();
+        // GET: Home 
+        public ActionResult Index()
+        {
+            return View(_db.Blogs.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude ="Id")]Blog blogToCreate)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            _db.Blogs.Add(blogToCreate);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+               
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
